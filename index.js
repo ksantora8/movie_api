f13500408d759c316c00de1b07050195f9f54949
ksantora8 <kathsantora@gmail.com>
2021-05-21 17:50:01 -0400

    Imported express and morgan, removed unneeded file, moved documentation html to public folder, created route with text response for end point /, create route with json response for endpoint /movies, added error handling middleware, logging requests using morgan

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express'),
+      morgan = require ('morgan'),
+      app = express();
+
+app.use(morgan('common'));
+app.use(express.static('/public'));
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.get('/movies',(req,res) => {
+  res.json([
+    {movie1:'info1'},
+    {movie2:'info2'},
+    {movie3:'info3'},
+    {movie4:'info4'},
+    {movie5:'info5'},
+    {movie6:'info6'},
+    {movie7:'info7'},
+    {movie8:'info8'},
+    {movie9:'info9'},
+    {movie10:'info10'}
+  ]);
+});
+
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
