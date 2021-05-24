b15783fdf407a42d82e009658bf546b9004c7e49
ksantora8 <kathsantora@gmail.com>
2021-05-24 16:28:49 -0400

    Added in-memory array of movies, changed get request to list the array

--- index.js
+++ index.js
@@ -5,29 +5,82 @@ const express = require('express'),
       app = express();
 
 app.use(morgan('common'));
+app.use(bodyParser.json());
 app.use(express.static('/public'));
 //app.use((err, req, res, next) => {
 //  console.error(err.stack);
   //res.status(500).send('Something broke!');
 //});
+let movies = [
+  {
+    title: 'Us',
+    year: 2019,
+    director: 'Jordan Peele',
+    genres:['horror','thriller']
+  },
+  {
+    title: 'Get Out',
+    year: 2017,
+    director: 'Jordan Peele',
+    genres:['horror','thriller']
+  },
+  {
+    title: 'A Quiet Place',
+    year: 2018,
+    director: 'John Krazinski',
+    genres:['horror','thriller']
+  },
+  {
+    title: 'The Cabinet of Doctor Calgari',
+    year: 1921,
+    director: 'Robert Wiene',
+    genres:['horror']
+  },
+  {
+    title: 'Nosferatu',
+    year: 1922,
+    director: 'F.W. Murnau',
+    genres:['horror']
+  },
+  {
+    title:'King Kong',
+    year: 1933,
+    director: ['Merian C. Cooper', 'Ernest B. Schoedsack'],
+    genres:['adventure', 'fantasy']
+  },
+  {
+    title: 'Psycho',
+    year: 1960,
+    director: 'Alfred Hitchcock',
+    genres:['horror', 'thriller']
+  },
+  {
+    title:'The Invisible Man',
+    year: 2020,
+    director: 'Leigh Whannell',
+    genres:['horror', 'thriller']
+  },
+  {
+    title:'The Bride of Frankenstein',
+    year: 1935,
+    director: 'James Whale',
+    genres:['horror']
+  },
+  {
+    title: 'The Babadook',
+    year: 2014,
+    director: 'Jennifer Kent',
+    genres:['horror','thriller']
+  }
+]
+
 
 app.get('/', (req, res) => {
   res.send('Welcome to my app!');
 });
 
 app.get('/movies',(req,res) => {
-  res.json([
-    {movie1:'info1'},
-    {movie2:'info2'},
-    {movie3:'info3'},
-    {movie4:'info4'},
-    {movie5:'info5'},
-    {movie6:'info6'},
-    {movie7:'info7'},
-    {movie8:'info8'},
-    {movie9:'info9'},
-    {movie10:'info10'}
-  ]);
+  res.json(movies);
 });
 
 app.get('/documenation.html', (req, res) => {
